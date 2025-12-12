64e0c84cb36de381bad8a0a791146c743c79a4f8
Tony Hallett <tonyhallett74@gmail.com>
2025-12-12 10:49:06 +0000

    temp remove router

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { Route, HashRouter as Router, Routes } from "react-router";
+// import { Route, HashRouter as Router, Routes } from "react-router";
 import { App } from "./App";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
-root.render(
-  <Router>
+root.render(<App />);
+{
+  /* <Router>
     <Routes>
       <Route path="/" element={<App />}></Route>
     </Routes>
-  </Router>
-);
+  </Router> */
+}
