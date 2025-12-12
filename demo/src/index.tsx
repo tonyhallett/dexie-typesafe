43d9bbcb5b49688e5b2b3f82954e7ebf4b227de2
Tony Hallett <tonyhallett74@gmail.com>
2025-12-12 10:47:04 +0000

    demo setup

---
+++ demo/src/index.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import { Route, HashRouter as Router, Routes } from "react-router";
+import { App } from "./App";
+
+const container = document.getElementById("app");
+const root = createRoot(container!);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+  </Router>
+);
